2de2e388eec5ed24f381c410653517ccbd9b118c
Bo Meng <mengbo@hotmail.com>
2018-06-02 08:36:47 -0700

    initial checkin

---
+++ src/com/bomeng/solution5/Solution.java
@@ -0,0 +1,15 @@
+package com.bomeng.solution5;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+    }
+
+    public String longestPalindrome(String s) {
+
+    }
+}
