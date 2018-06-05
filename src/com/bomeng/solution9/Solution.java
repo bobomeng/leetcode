830658d3f13aad084c55569e0dbc5720a2e13716
Bo Meng <mengbo@hotmail.com>
2018-06-04 21:38:36 -0700

    change

--- src/com/bomeng/solution9/Solution.java
+++ src/com/bomeng/solution9/Solution.java
@@ -7,9 +7,25 @@ public class Solution {
     public static void main(String[] args) {
         Solution s = new Solution();
 
+        System.out.println(s.isPalindrome(121));
+        System.out.println(s.isPalindrome(-121));
+        System.out.println(s.isPalindrome(10));
     }
 
     public boolean isPalindrome(int x) {
-
+        if (x < 0) {
+            return false;
+        } else if (x < 10) {
+            return true;
+        } else {
+            int value = 0;
+            int input = x;
+            while (input != 0) {
+                int d = input % 10;
+                value = value * 10 + d;
+                input = input / 10;
+            }
+            return value == x;
+        }
     }
 }
