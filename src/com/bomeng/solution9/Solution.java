61465dba3cbf6b896dd0322031c415d7b52f5875
Bo Meng <mengbo@hotmail.com>
2018-06-04 21:28:55 -0700

    initial checkin

---
+++ src/com/bomeng/solution9/Solution.java
@@ -0,0 +1,15 @@
+package com.bomeng.solution9;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+    }
+
+    public boolean isPalindrome(int x) {
+
+    }
+}
