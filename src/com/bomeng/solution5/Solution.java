e96c4964ec31faff77c0a160ad96662dc042fe1d
Bo Meng <mengbo@hotmail.com>
2018-06-08 09:14:54 -0700

    .

--- src/com/bomeng/solution5/Solution.java
+++ src/com/bomeng/solution5/Solution.java
@@ -1,8 +1,5 @@
 package com.bomeng.solution5;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Solution {
     public static void main(String[] args) {
         Solution s = new Solution();
